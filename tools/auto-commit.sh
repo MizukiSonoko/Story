b8327f3535c5a264985896b460629ada549e3c7c
mizukisonoko <mizuki.sonoko@gmail.com>
2017-08-14 13:22:19 +0900

    Update deploy script

--- tools/auto-commit.sh
+++ tools/auto-commit.sh
@@ -3,9 +3,10 @@
 git config --global user.email "mizuki.sonoko@gmail.com"
 git config --global user.name  "mizukisonoko"
 
-DATE=`date +"v%Y%m%d-%H:%M:%S"`
-git add *
+DATE=`date +"v%Y/%m/%d-%H:%M:%S"`
 git checkout -b snapshot
+git add *
 git commit -m "[auto] Update Papar (${DATE})"
+git push --delete "git@github.com:${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}.git" snapshot
 git push "git@github.com:${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}.git" snapshot
 
