e42952f9908befb58f1d58a2d4a24530958c0b9a
mizukisonoko <mizuki.sonoko@gmail.com>
2017-08-14 11:37:08 +0900

    Add install and gitignore

--- tools/auto-commit.sh
+++ tools/auto-commit.sh
@@ -4,7 +4,6 @@ git config --global user.email "mizuki.sonoko@gmail.com"
 git config --global user.name  "mizukisonoko"
 
 DATE=`date +"v%Y%m%d-%H:%M:%S"`
-echo $DATE >> terraform.md
 git add *
 git checkout -b snapshot
 git commit -m "[auto] Update Papar (${DATE})"
